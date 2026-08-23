53012e35eda1388bf14d55caa8634face55755e3
AmankMeena289 <amankumarm289@gmail.com>
2026-08-24 01:30:07 +0530

    Connection to MOngoDB atlas

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import dns from "dns";
+import connectDB from "../db/index.js";
+
+dotenv.config();
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+console.log("MONGODB_URI exists:", !!process.env.MONGODB_URI);
+console.log("DB_NAME:", process.env.DB_NAME);
+
+connectDB();
